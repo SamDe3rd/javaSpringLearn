289cdcc5f885ded2bd0149b4f9a6bfe2ff943933
Sam3 <stamfordsees@gmail.com>
2025-06-22 06:02:40 +0800

    phase 1 webclient

--- src/main/java/com/spring/springLearn/config/WebClientConfig.java
+++ src/main/java/com/spring/springLearn/config/WebClientConfig.java
@@ -1,10 +1,8 @@
 package com.spring.springLearn.config;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 @Configuration
 public class WebClientConfig {
--- src/main/java/com/spring/springLearn/controller/FunApi.java
+++ src/main/java/com/spring/springLearn/controller/FunApi.java
@@ -2,7 +2,6 @@ package com.spring.springLearn.controller;
 
 import com.spring.springLearn.model.FunApiResponse;
 import com.spring.springLearn.service.FunService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
