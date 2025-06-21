ead7269a5c4a5568c88c603216182da7e346875d
Sam3 <stamfordsees@gmail.com>
2025-06-22 04:10:40 +0800

    init

---
+++ src/test/java/com/spring/springLearn/SpringLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
